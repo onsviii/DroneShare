e91e93d0b47c03321f20c6393fb5d114bc02e0de
onsviii <svitiashchuknazar@gmail.com>
2025-11-22 00:23:09 +0200

    Initial commit

---
+++ src/test/java/dev/onsvii/droneshare/DroneShareApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.onsvii.droneshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
