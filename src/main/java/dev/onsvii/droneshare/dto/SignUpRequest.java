4a945a497b5dc9fb22701fc82c1f30f110b41f83
onsviii <svitiashchuknazar@gmail.com>
2025-11-28 11:43:02 +0200

    rename UserRegistrationDTO to SignUpRequest

--- src/main/java/dev/onsvii/droneshare/dto/SignUpRequest.java
+++ src/main/java/dev/onsvii/droneshare/dto/SignUpRequest.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserRegistrationDTO {
+public class SignUpRequest {
     @NotBlank(message = "Username cannot be empty")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
