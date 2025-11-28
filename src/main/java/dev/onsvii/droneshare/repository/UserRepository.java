db6c936a0d39a986bafcf12ef0bf6315b29b4053
onsviii <svitiashchuknazar@gmail.com>
2025-11-28 11:43:18 +0200

    add findByUsernameOrEmail method to UserRepository

--- src/main/java/dev/onsvii/droneshare/repository/UserRepository.java
+++ src/main/java/dev/onsvii/droneshare/repository/UserRepository.java
@@ -10,6 +10,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameOrEmail(String username, String email);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
+    boolean existsByUsernameOrEmail(String username, String email);
 }
