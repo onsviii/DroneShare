1401e73d503fece10aeb04a044a605327e2d54ef
onsviii <svitiashchuknazar@gmail.com>
2025-11-28 11:04:53 +0200

    add validation to user registration DTO

--- src/main/java/dev/onsvii/droneshare/dto/UserRegistrationDTO.java
+++ src/main/java/dev/onsvii/droneshare/dto/UserRegistrationDTO.java
@@ -1,5 +1,9 @@
 package dev.onsvii.droneshare.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRegistrationDTO {
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Invalid phone number")
     private String phone;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 }
