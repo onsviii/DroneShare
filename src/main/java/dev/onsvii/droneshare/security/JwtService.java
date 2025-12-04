54b6cf7322d7c44c695e519e7b4a85eb2541aa64
onsviii <svitiashchuknazar@gmail.com>
2025-12-04 20:26:18 +0200

    upgrade JwtService to support JJWT 0.13.x API

--- src/main/java/dev/onsvii/droneshare/security/JwtService.java
+++ src/main/java/dev/onsvii/droneshare/security/JwtService.java
@@ -2,14 +2,13 @@ package dev.onsvii.droneshare.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -37,7 +36,6 @@ public class JwtService {
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUsername(token);
-
         return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
@@ -55,7 +53,7 @@ public class JwtService {
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .signWith(getSigningKey(), Jwts.SIG.HS256)
                 .compact();
     }
 
@@ -63,21 +61,21 @@ public class JwtService {
         return extractExpiration(token).before(new Date());
     }
 
-
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
         final Claims claims = extractAllClaims(token);
-
         return claimsResolvers.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
-                .getBody();
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
-    private Key getSigningKey() {
+    private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
-
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
